a2c475eecd09511e7e401fdbc91dd39f8a7ad34f
germainep <glpannell@icloud.com>
2016-12-31 20:59:34 -0500

    decimal bug fix
    fixed a bug where pressing the decimal key multiple times would break calculations.
    Also moved number parsing to the Screen service.

--- src/js/calc-services.js
+++ src/js/calc-services.js
@@ -1,35 +1,37 @@
 Calc.service('ScreenService',['$rootScope', 'KeymapService', function($rootScope, KeymapService){
   this.calMem = [];
   this.toScreen = 0;
+  this.decimal = false;
   this.result = 0;
+  this.parser = function(temp) {
+    return parseFloat(temp.join(''));
+  }
   this.temp = [];
   this.operation = function(event){
     if(this.calMem.length === 2){
-
       this.result = eval('' + this.calMem[0] + this.calMem[1] + this.toScreen + '');
       this.calMem[0] = this.result;
-      this.calMem[1] = KeymapService[event.target.id];
+      this.calMem[1] = KeymapService.allowedTargets[event.target.id];
       this.temp= [];
       this.toScreen = this.result;
-
       $rootScope.$digest();
       }else {
         this.temp = [];
         this.calMem[0] = this.toScreen;
-        this.calMem[1] = KeymapService[event.target.id];
-        console.log(this.calMem);
+        this.calMem[1] = KeymapService.allowedTargets[event.target.id];
       }
   };
 }]);
 
-Calc.factory('KeymapService', [function() {
-    var allowedTargets = {
+Calc.service('KeymapService', [function(event) {
+    this.allowedTargets = {
       keyClear: 'clear',
       keyMinus: '-',
       keyPlus: '+',
       keyDivide: '/',
       keyMultiply: '*',
       keyDecimal: '.',
+      numbers: {
       key1: 1,
       key2: 2,
       key3: 3,
@@ -41,5 +43,5 @@ Calc.factory('KeymapService', [function() {
       key9: 9,
       key0: 0
     }
-    return allowedTargets
+    }
 }]);
