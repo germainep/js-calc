92e07e35e4c4c1f9ab99af51a3b029b953037a1d
germainep <glpannell@icloud.com>
2016-12-31 20:59:34 -0500

    decimal bug fix
    fixed a bug where pressing the decimal key multiple times would break calculations.
    Also moved number parsing to the Screen service.

--- src/js/calc-controllers.js
+++ src/js/calc-controllers.js
@@ -1,52 +1,59 @@
 Calc.controller('ScreenController',['$scope', 'KeymapService', 'ScreenService', function($scope, KeymapService, ScreenService){
   $scope.screen = 0;
   document.addEventListener('click', function(event) {
-    console.log(event.target.id);
     if(event.target.id === 'keyPlus'){
       ScreenService.operation(event);
-      console.log(ScreenService.toScreen);
       $scope.screen = ScreenService.toScreen;
+      ScreenService.decimal = false;
       $scope.$digest();
 
     } else if(event.target.id === 'keyMinus') {
       ScreenService.operation(event);
       $scope.screen = ScreenService.toScreen;
+      ScreenService.decimal = false;
       $scope.$digest();
 
     }else if(event.target.id === 'keyMultiply') {
       ScreenService.operation(event);
       $scope.screen = ScreenService.toScreen;
+      ScreenService.decimal = false;
       $scope.$digest();
 
     }else if(event.target.id === 'keyDivide') {
       ScreenService.operation(event);
       $scope.screen = ScreenService.toScreen;
+      ScreenService.decimal = false;
       $scope.$digest();
 
     }else if(event.target.id === 'keyClear') {
     ScreenService.calMem = [];
     ScreenService.temp = [];
     ScreenService.toScreen = 0;
+    ScreenService.decimal = false;
     $scope.screen = ScreenService.toScreen;
     $scope.$digest();
-    console.log(ScreenService);
+
+  }else if(event.target.id === 'keyDecimal') {
+    if(!ScreenService.decimal){
+    ScreenService.temp.push('.');
+    ScreenService.decimal = true;
+    }
 
   }else if(event.target.id === 'keyEqual' && ScreenService.calMem.length === 2){
-    console.log(ScreenService.toScreen);
+    (ScreenService.toScreen);
     ScreenService.result = eval('' + ScreenService.calMem[0] + ScreenService.calMem[1] + ScreenService.toScreen + '');
     ScreenService.calMem[0] = ScreenService.result;
     ScreenService.calMem.splice(1,1);
     ScreenService.temp= [];
+    ScreenService.decimal = false;
     ScreenService.toScreen = ScreenService.result;
     $scope.screen = ScreenService.toScreen;
-    console.log(ScreenService.calMem);
     $scope.$digest();
 
-  }else {
-    ScreenService.temp.push(KeymapService[event.target.id]);
-    ScreenService.toScreen = parseFloat(ScreenService.temp.join(''));
+  }else if(KeymapService.allowedTargets.numbers[event.target.id]){
+    ScreenService.temp.push(KeymapService.allowedTargets.numbers[event.target.id]);
+    ScreenService.toScreen = ScreenService.parser(ScreenService.temp);
     $scope.screen = ScreenService.toScreen;
-    console.log(ScreenService.toScreen);
     $scope.$digest();
   }
   })
